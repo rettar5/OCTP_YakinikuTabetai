cb6d22e2336c3cff2f632db1f7e0fa118e25d3e6
rettar5 <rettar.v@gmail.com>
2018-03-02 12:17:19 +0900

    RTに反応しないよう修正

--- yakinikuTabetai.ts
+++ yakinikuTabetai.ts
@@ -30,7 +30,7 @@ export class YakinikuTabetai {
    * @returns {boolean}
    */
   static isValid(tweetData: OdnTweetData): boolean {
-    return tweetData.text.match(/^(他)?人の(お)?(金|かね|財布)で(焼)?(き)?(肉|にく)(が|を)?(食べたい|たべたい)$/gi) ? true : false;
+    return false === tweetData.isRetweet && tweetData.text.match(/^(他)?人の(お)?(金|かね|財布)で(焼)?(き)?(肉|にく)(が|を)?(食べたい|たべたい)$/gi) ? true : false;
   }
 }
 
